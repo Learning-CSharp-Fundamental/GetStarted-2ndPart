11b78c5657a7b4071e007e1dede72e874a02b8f3
Reza Qadimi <rezaqadimi.ir@gmail.com>
2021-06-10 19:03:47 +0430

    Add project files.

---
+++ MyApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
+
+//namespace MyApplication
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			// Note: Compile Error!
+//			Console.WriteLine("Hello World!");
+//		}
+//	}
+//}
